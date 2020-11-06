59c4135f965dc9374859c7d52e176900b19a20f3
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-06 23:59:23 +0100

    workoutViews config

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,27 @@ export const router = createRouter({
             path: '/',
             name: 'Home',
             component: () => import('@/views/Home.vue')
-        }
+        },
+        {
+            path: '/get_started',
+            name: 'GetStarted',
+            component: () => import('@/views/GetStarted.vue')
+        },
+        {
+            path: '/beginner',
+            name: 'Beginner',
+            component: () => import('@/workoutViews/beginner.vue')
+        },
+        {
+            path: '/advance',
+            name: 'Advance',
+            component: () => import('@/workoutViews/advance.vue')
+        },
+        {
+            path: '/pro',
+            name: 'Pro',
+            component: () => import('@/workoutViews/pro.vue')
+        },
     ],
     history: createWebHistory()
 })

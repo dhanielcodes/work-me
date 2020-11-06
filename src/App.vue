24355ed27d9e3c3396a356816d4178a09d880ef8
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-06 08:37:43 +0100

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <Nav />
-    <router-view />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Nav from "@/components/Nav.vue";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  components:{
-    Nav
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
 
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap');
-*{
-  font-family: 'inter';
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>

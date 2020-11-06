7b1126294c0b60a4ad8e9ff043b7bf54f2ee372f
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-06 08:51:32 +0100

    rooter setup

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export const router = createRouter({
+    routes:[
+        {
+            path: '/',
+            name: 'Home',
+            component: () => 
+                import('../views/Home.vue')
+        }
+    ],
+    history: createWebHistory()
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        home
+        <h1>Home</h1>
     </div>
 </template>
 

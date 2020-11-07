b4b8c3c756e8aa6b5a2c23d54854872662347bba
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-07 16:40:20 +0100

    beginner

--- src/workoutViews/beginner.vue
+++ src/workoutViews/beginner.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="bg"></div>
-        <img src="../assets/bg_line_green.svg" alt="" class="side_bg1">
-        <img src="../assets/bg_line_yellow.svg" alt="" class="side_bg2">
+        <img src="../assets/bg_line_green.svg" alt="" class="side_bg1 side">
+        <img src="../assets/bg_line_yellow.svg" alt="" class="side_bg2 side">
 
         <div class="main" v-if="session">
             <ul v-for="(item, index) in store.state.questions.slice(a, b)" :key="index">
@@ -11,19 +11,24 @@
                     <h2>{{item.amount}}</h2>
                     <p>{{item.info}}</p>
                 </li>
-                <button @click="selectResponse(item)" v-on:click="next" :class="select ? check(item) : ''">Done</button>
-                <button v-on:click="next">skip</button>
+                <div class="btns">
+                    <button @click="selectResponse(item)" v-on:click="next" :class="select ? check(item) : ''">Done</button>
+                    <button v-on:click="next">Skip</button>
+                </div>
             </ul>
             <div class="bared">
                 <div class="bar" :style="{ 'width' : bar + '%'}"></div>
             </div>
         </div>
         <div class="score" v-else>
-            <h1>You completd</h1>
-            {{score}}/{{store.state.questions.length}}
-            <button @click="restart">restart</button>
-            <button @click="mode">Change mode</button>
-            <button @click="home">Home</button>
+            <h1>You completed</h1>
+            <h2>{{score}}/{{store.state.questions.length}} Workouts</h2>
+            <div class="btns">
+                <button @click="restart">Restart</button>
+                <button @click="mode">Change mode</button>
+                <button @click="home">Home</button>
+            </div>
+         
         </div>
     </div>
 </template>
@@ -66,7 +71,7 @@ export default {
                 state.a++
                 state.b++
                 state.select = false
-                state.bar += 10
+                state.bar = state.bar + (100/store.state.questions.length)
         }
 
 
@@ -108,14 +113,103 @@ export default {
     top: 50%;
     transform: translateY(-50%);
     right:0;
+    width: 150px;
 }
 .side_bg2{
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
     left: 0;
-}.bar{
-    background: green;
-    height: 10px;
+    width: 150px;
+}
+ul{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); 
+    text-align: center;
+    list-style: none;
+    width: 80%;
+}
+ul h1, h2{
+    font-weight: 500;
+}
+ul p{
+    width: 90%;
+    margin: auto;
+    margin-top: 40px;
+}
+.btns{
+    margin-top: 50px;
+}
+.btns button{
+    padding: 20px;
+    font-size: 20px;
+    border: none;
+    margin-right: 10px;
+    cursor: pointer;
+    color: white;
+    border-radius: 10px;
+    outline: none;
+}
+.btns button:nth-child(1){
+    background: #3DFC44;
+}
+.btns button:nth-child(2){
+    background: #EE7272;
+}
+
+.bared{
+    width: 70%;
+    background: rgba(197, 191, 191, 0.514);
+    height: 30px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 20px 20px 0px 0px;
+    overflow: hidden;
+}
+.bar{
+    background: #3DFC44;
+    height: 100%;
+    width: 0%;
+    transition: all .3s;
+}
+/* SCORE */
+
+.score{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); 
+    text-align: center;
+    list-style: none;
+    width: 80%;
+}
+.score .btns button{
+    font-size: clamp(13px, 3vw, 20px);
+    padding: 10px;
+}
+.score .btns button:nth-child(1){
+    background: #90A9FF;
+}
+.score .btns button:nth-child(2){
+    background: #000000;
+}
+.score .btns button:nth-child(3){
+    background: #34CEFF;
+}
+.score h1{
+    font-weight: 500;
+    font-size: 30px;
+}
+.score h2{
+    margin-top: 40px;
+}
+@media (max-width: 900px){
+    .side{
+        display: none;
+    }
 }
 </style>
